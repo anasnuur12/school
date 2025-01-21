aaa1670b7d436ef887cf4389853134215bcdba57
anasnuur12 <hawushamaanta@gmail.com>
2025-01-20 22:34:06 -0800

    Delete loginout.php

--- loginout.php
+++
@@ -1,7 +0,0 @@
-<script>
-    window.onpageshow = function(event) {
-        if (event.persisted || (window.performance && window.performance.navigation.type === 2)) {
-            window.location.href = 'login.php'; // Redirect if back button is used
-        }
-    };
-</script>
